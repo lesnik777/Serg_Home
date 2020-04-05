4e6b704218da9666fab2bf3cef4d08b8008103e3
Serg ink <serg.ink777@gmail.com>
2020-04-06 00:10:37 +0300

    added "Convert boolean values to strings Yes or No." file

---
+++ Convert boolean values to strings 'Yes' or 'No'.py
@@ -0,0 +1,15 @@
+def bool_to_word(boolean):
+    # TODO
+     if  boolean== True:
+         return 'Yes' 
+     else:
+        return'No'
+      
+print(bool_to_word(boolean))
+###########################
+
+def bool_to_word(boolean):
+
+    return 'Yes' if boolean == True else 'No'
+
+print(bool_to_word(boolean))
